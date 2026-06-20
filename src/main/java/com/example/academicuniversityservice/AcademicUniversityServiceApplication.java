0c868fe7cd06391065828570b037b844e5d45c67
Juan David Sevilla <sevilla.juan@correounivalle.edu.co>
2026-06-19 20:18:17 -0500

    feat(initial commit)

---
+++ src/main/java/com/example/academicuniversityservice/AcademicUniversityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.academicuniversityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicUniversityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicUniversityServiceApplication.class, args);
+    }
+
+}
